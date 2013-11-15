d053ea1e90c89f7aa1c4b95f2fd8c522ecdf108b
pbojinov <petarbojinov@gmail.com>
2013-11-15 09:31:14 -0800

    adding local storage

--- app/scripts/menuItems.js
+++ app/scripts/menuItems.js
@@ -15,6 +15,7 @@ console.log('init images on wheel content script');
 
 var WOW = {};
 
+WOW.Cache = {};
 WOW.App = function() {
 
     var numberResults = 3,
@@ -25,10 +26,37 @@ WOW.App = function() {
             w: 150,
             h: 150
         },
-        $menuItems = $('.addItem');
+        $menuItems = $('.addItem'),
+        menuId;
 
     function init() {
+        parseMenuId();
+        getCache();
         parseMenuItemText();
+    }
+
+    /**
+     * We will use the menu id as the key for urls in cache
+     * http://www.waitersonwheels.com/restaurants/menu/169
+     * => menu/169
+     */
+
+    function parseMenuId() {
+        var url = window.location.href,
+            a = document.createElement('a')
+
+            a.href = url;
+        menuId = a.pathname.replace('/restaurants/', '');
+    }
+
+    function getCache() {
+        chrome.storage.sync.get({
+            'WOW.Cache'
+        }, function(items) {
+            // Notify that we saved.
+            console.log('Settings retrieved' + items);
+            WOW.Cache = items;
+        });
     }
 
     function parseMenuItemText() {
@@ -36,8 +64,11 @@ WOW.App = function() {
         $.each($menuItems, function(index, item) {
             menuItemText = item.innerHTML;
             console.log(menuItemText, item);
-            makeAPICall(menuItemText, item);
+            //makeAPICall(menuItemText, item);
         });
+
+        //Save our requests in local storage
+
         //are all requests done processing?
         //might need to make 160 promises
         initializeMagnificPopup();
@@ -45,10 +76,10 @@ WOW.App = function() {
 
     function makeAPICall(queryString, childNode) {
         //encode query string
-        queryString = encodeURI(queryString);
-        console.log(queryString);
+        encodedQueryString = encodeURI(queryString);
+        console.log(encodedQueryString);
 
-        var finalAPIUrl = googleAPI.url + queryString;
+        var finalAPIUrl = googleAPI.url + encodedQueryString;
         console.log(finalAPIUrl);
 
         var xhr = new XMLHttpRequest();
@@ -57,7 +88,7 @@ WOW.App = function() {
             if (xhr.readyState === 4) {
                 if (xhr.status === 200) {
                     console.log('success', xhr.responseText);
-                    processImageResponse(JSON.parse(xhr.responseText), childNode);
+                    processImageResponse(JSON.parse(xhr.responseText), queryString, childNode);
                 } else {
                     console.log('Error', xhr.statusText);
                 }
@@ -66,14 +97,14 @@ WOW.App = function() {
         xhr.send();
     }
 
-    function processImageResponse(response, childNode) {
+    function processImageResponse(response, queryString, childNode) {
         var $parentNode = $(childNode).parent();
 
         //iterate through number of results (numberResults)
         //response.responseData.results[0].url;
         $.each(response.responseData.results, function(index, item) {
             var img = document.createElement('img'),
-            	a = document.createElement('a'),
+                a = document.createElement('a'),
                 lineBreak = document.createElement('br'),
                 imgSrc = item.url;
 
@@ -81,7 +112,9 @@ WOW.App = function() {
                 $parentNode.prepend(lineBreak);
                 console.log('prepend line break');
             }
-        	a.href = imgSrc;
+            WOW.Cache[menuId][queryString] = item.url;
+
+            a.href = imgSrc;
             img.width = thumbnailDimensions.w,
             img.height = thumbnailDimensions.h;
             img.className = 'magnificGalleryItem'
@@ -91,24 +124,37 @@ WOW.App = function() {
     }
 
     function initializeMagnificPopup() {
-
-        // //popup modal for img
-        // $('img').magnificPopup({
-        //     type: 'image',
-        //     mainClass: 'mfp-fade',
-        //     removalDelay: 160,
-        //     preloader: false,
-        //     fixedContentPos: false,
-        //     closeOnContentClick: false
-        // });
-
-        // This will create a single gallery from all elements that have class "gallery-item"
+        //Create a single gallery for images
         $('#pcon').magnificPopup({
-        	delegate: 'a',
+            delegate: 'a',
             type: 'image',
             gallery: {
                 enabled: true
-            }
+            },
+            preloader: false,
+            fixedContentPos: false,
+            closeOnContentClick: true,
+            removalDelay: 160,
+            mainClass: 'mfp-fade'
+        });
+    }
+
+    function saveKey(key, value) {
+        // Save it using the Chrome extension storage API.
+        chrome.storage.sync.set({
+            key: value
+        }, function() {
+            // Notify that we saved.
+            console.log('Settings saved');
+        });
+    }
+
+    function getGet(key) {
+        chrome.storage.sync.get({
+            key
+        }, function(items) {
+            // Notify that we saved.
+            console.log('Settings retrieved' + items);
         });
     }
 
